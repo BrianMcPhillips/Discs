c8cc625c548d62722564854f0cf63b4bed701be8
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-12 21:36:10 -0700

    Add appropriate commenting

--- src/components/ListPage/List/Item/Item.jsx
+++ src/components/ListPage/List/Item/Item.jsx
@@ -3,6 +3,7 @@ import styles from './Item.module.css';
 
 export default class Item extends Component {
   render() {
+    //Destructure props
     const { 
       data: {
         brand,
@@ -12,6 +13,7 @@ export default class Item extends Component {
       } 
     } = this.props;
     return (
+      //List of relevant info for each item being rendered to page
       <div className={styles.item}>
         <img src={image} alt={name} />
         <h3>{name}</h3>
--- src/components/ListPage/List/List.jsx
+++ src/components/ListPage/List/List.jsx
@@ -4,8 +4,10 @@ import styles from './List.module.css';
 
 export default class List extends Component {
   render() {
+    //Destructure Props
     const { data } = this.props;
     return (
+      // Map over props data to dynamically render items to page
       <div className={styles.list}>
         {
           data.map(item => <Item key={item.name} data={item}/>)
