6ae4734a0b225121f3c5d88088522e18b501d714
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-15 02:27:17 -0700

    Router setup, Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import ListPage from './ListPage/ListPage';
+import Header from './Header/Header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ListPage /> 
+        <Router>
+          <Header /> 
+          <Switch>
+            <Route
+              path='/'
+              exact
+              render={(routerProps) => <ListPage {...routerProps}/>}
+            />
+          </Switch>
+        </Router>
       </div>
     )
   }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        Disc Golf Bag
+      </div>
+    )
+  }
+}
